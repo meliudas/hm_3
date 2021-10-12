27aabdd7b26817abd183f3e0b691a001d54f83e8
meliudas <batyanite@gmail.com>
2021-10-12 20:57:32 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+dateOfBirthday = 2004 # camel case
+date_of_birthday = 2005 # snake case
+var_int = 2  # Variable integer(число)
+var_str = "nadir"  # Variable string(строка
+var_boolean_true = True  # Variable boolean(истина)
+var_boolean_false = False  # Variable boolean(ложь)
+var_list = [1, 2, 3, "nadir", [1, 2, 3, ], (1, 2, 3,), ]
+var_tuple = (1, [1, 2, ], "Nadir",)  # Variable tuple(коротеж)
+var_dict = {"name": "nadir", "date_of_birthday": "02.02.2005", "course": 3,
+            "physical_data": [80, 180], "python": True}  # Variable dictionary(словарь)
+
+
+
+def print_hi(name):
+  dateOfBirthday = 2004  # camel case
+  date_of_birthday = 2005  # snake case
+  var_int = 2  # Variable integer(число)
+  var_str = "nadir"  # Variable string(строка
+  var_boolean_true = True  # Variable boolean(истина)
+  var_boolean_false = False  # Variable boolean(ложь)
+  var_list = [1, 2, 3, "nadir", [1, 2, 3, ], (1, 2, 3,), ]
+  var_tuple = (1, [1, 2, ], "Nadir",)  # Variable tuple(коротеж)
+  var_dict = {"name": "nadir", "date_of_birthday": "02.02.2005", "course": 3,
+              "physical_data": [80, 180, ], "python": True}  # Variable dictionary(словарь)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
